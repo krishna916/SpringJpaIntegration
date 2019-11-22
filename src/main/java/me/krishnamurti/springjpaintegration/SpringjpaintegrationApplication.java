36c7c352774d626e46fefaad1896d87cedab348e
krishna916 <krishna916@outlook.com>
2019-11-22 09:55:06 +0530

    Initial Project Set up.
    Added Dependencies

---
+++ src/main/java/me/krishnamurti/springjpaintegration/SpringjpaintegrationApplication.java
@@ -0,0 +1,13 @@
+package me.krishnamurti.springjpaintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpaintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/krishnamurti/springjpaintegration/SpringjpaintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package me.krishnamurti.springjpaintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpaintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
